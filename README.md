151aa264ee3c3221a6dd18374b508e3ba06ea184
Anthony D'Achille <adachille3@gatech.edu>
2019-11-13 16:16:43 -0500

    Modify story and action sequence 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # if-game-engine
 This repository holds my own interactive fiction game engine coded in c++.
+
+To build the game, simply run the make in the root directory.
--- engine.cpp
+++ engine.cpp
@@ -86,20 +86,20 @@ void PlayGame()
 {
     Setup();
 
-    // Print introduction scene
+    // Story Sequence 1 //
     std::cout << "You've woken up.\n\n";
-    std::cout << "Everything is black. There is no sound. You cannot move...\n\n";
-    std::cout << "The void in front of you slowly begins to vibrate, then tremble, and finally, boil. You see three dark green spikes slowly emerge from the vigorously churning bubbles...\n\n";
-    std::cout << "They continue to grow upwards and begin twisting in on themselves, like overgrown nails...\n\n";
-    std::cout << "The mass is now forced outwards by its own growth, inching closer and closer to you. But you cannot move...\n\n";
-    std::cout << "The first green spike makes contact with your chest, pushing it's edge into your body. There is no sound. There is no pain nor blood. The spike slowly pushes its way out through your back. You notice the spike's growth slowly die down. There is now only the slow vibration of the mass back and forth... back and forth...\n\n";
-    std::cout << "The dark green mass stops entirely. The spike is only sticking a couple inches out of your back. You feel control slowly flow back into your body...\n\n";
+    std::cout << "Everything around you is pitch black, only your body is illuminated. You can see your arms, legs, and feet but you are completely paralyzed and cannot move. There is only silence and stillness...\n\n";
+    std::cout << "A short distance in front of you, a small dark green circle appears. The dot slowly grows larger and you begin to notice an almost imperceptible vibration... It grows in movement until it reaches tremble... and finally a boil. From the stew, You see three dark green spikes slowly emerge from the vigorously churning bubbles...\n\n";
+    std::cout << "The spikes grow upwards until their weight causes them to begin twisting in on themselves, like overgrown nails...\n\n";
+    std::cout << "The mass is now forced outwards by its own growth, towards you... It inches closer and closer, but you cannot move...\n\n";
+    std::cout << "You look down as the first green spike gently touches your chest. It continues moving, pushing it's edge into your body with no hesitation. There is no sound. There is no pain nor blood. The spike slowly pushes its way forward and out through your back. But, you notice the boil start to die down, and with it, the spike's growth grinds to a halt. You feel it's slow vibration in your chest... Finally, it comes to stop.\n\n";
+    std::cout << "The spike is only sticking a couple inches out of your back. You suddenly feel control flow back into your body...\n\n";
 
     // Action Sequence 1 //
     // Define Actions
     struct Action action1;
     action1.optionMessage = "push down on the spike and hoist yourself off of it.";
-    action1.resultMessage = "You struggle to push your body off the spike...\n\nThe spike is halfway out of your body when you notice that the black void that once supported your weight is now gone. With your feet freely dangling, you no longer want to push yourself off the spike. Panicking you remove your hands from the green mass. The end promptly snaps, and you begin to fall through the void...";
+    action1.resultMessage = "You struggle to push your body off the spike...\n\n The spike is halfway out of your body when it suddenly snaps, and you begin to fall through the void...";
     action1.actType = PROGRESSIVE;
     struct Action action2;
     action2.optionMessage = "kick your feet and yell at the void.";
@@ -107,7 +107,7 @@ void PlayGame()
     action2.actType = CYCLIC;
     struct Action action3;
     action3.optionMessage = "take a nap.";
-    action3.resultMessage = "With no sensation of pain, no dripping blood, or sights to intrigue, you find your eyes begin to close. The insomnia that has plagued you all your life takes hold. The black void becomes darker...\n\nWhen you open your eyes, you find yourself at a desk in a small gray cubicle. The bulky computer in front of you displays the Microsoft Windows 95 logo. You glance around for a moment, Tom is glaring at you. He's wearing overalls. You turn your head to back to your computer and open up Excel. You immediately save the file out of habit. You sigh and name the file 'Spreadsheet.xls'. Dreams are scarygit, you think to yourself.";
+    action3.resultMessage = "With no sensation of pain, no dripping blood, or sights to intrigue, you find your eyes begin to close. The insomnia that has plagued you all your life takes hold. The black void becomes darker...\n\nWhen you open your eyes, you find yourself at your familiar desk in your familiar small, grey cubicle. The bulky computer in front of you displays the Microsoft Windows 95 logo. You glance around for a moment, Tom is glaring at you. He's wearing overalls today. You avert your eyes and turn your head to back to the computer, opening up Excel. You immediately save the file, a habit built from many occasions of lost progress. You sigh and name the file 'Spreadsheet.xls'. Dreams are scary, you think to yourself.";
     action3.actType = FATAL;
     // Add actions and call Player Action
     std::vector<struct Action> actions;
@@ -115,6 +115,9 @@ void PlayGame()
     actions.push_back(action2);
     actions.push_back(action3);
     PlayerAction(actions);
+
+    // Story Sequence 2 //
+    
 
     // Action Sequence 2 //
 
